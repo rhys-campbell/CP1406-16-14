d800828cdc46fa91067fe904028940453e8c1b6f
camclark <cameron.clark1@my.jcu.edu.au>
2016-05-24 10:10:01 +1000

    Working as intended - contact
    as above

--- js/validate.js
+++ js/validate.js
@@ -2,7 +2,6 @@
  * Created by Cameron on 5/23/2016.
  */
 
-
 function validate() {
     var errorNode = this.parentNode.querySelector( ".error" ),
         span = document.createElement( "span" )
@@ -19,15 +18,21 @@ function validate() {
         span.innerHTML = this.getAttribute(
             this.validity.valueMissing ? "data-required-message" : "data-type-message" );
     }
-};
+}
 
 var form = document.querySelector( "form" ),
     inputs = form.querySelectorAll( "input" );
+    textareas = form.querySelectorAll("textarea");
 
 for ( var i = 0; i < inputs.length; i++ ) {
     inputs[ i ].addEventListener( "blur", validate );
     inputs[ i ].addEventListener( "invalid", validate );
-};
+}
+
+for ( var i = 0; i < textareas.length; i++ ) {
+    textareas[ i ].addEventListener( "blur", validate );
+    textareas[ i ].addEventListener( "invalid", validate );
+}
 
 // Turn off the bubbles
 form.addEventListener( "invalid", function( event ) {
@@ -42,3 +47,4 @@ document.querySelector( "form" ).addEventListener( "submit", function( event ) {
         event.preventDefault();
     }
 });
+0
